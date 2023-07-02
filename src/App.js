e55e6451b7f756a00f2d5d554496952739be82c9
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-02 14:14:09 -0700

    Adds react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-      <p>App</p>
+      <NavBar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar() {
+    return (
+        <div>
+            <p>NavBar Page</p>
+        </div>
+    )
+}
+
+export default NavBar;
