07c04c66843e43e3e299c166555d0dfb54f578ff
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-07 22:08:28 -0700

    Changes background colors

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div>
       <section>
         <h1>Journey Through the Minds of Great Philosophers</h1>
-        <p>Learn about the influential philosopher's who shaped our minds and the way we think today. <br></br> From Plato to Jacques Derrida, I hope you find the information you are searching for and more! Enlighten yourself!</p>
+        <p>Learn about the influential philosopher's who shaped our thoughts and the way we think today. <br></br> From Plato to Jacques Derrida, I hope you find the information you are searching for and more! Enlighten yourself!</p>
         <img src={homePageImage} alt="Socrates" className='homeImage'></img>
         <h3>Explore the ideas and philosophies that shaped our world</h3>
       </section>
