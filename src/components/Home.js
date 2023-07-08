66b20de4efb75c20b77c4aa2c3482af70bdbe777
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-08 14:48:43 -0700

    Adds content to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,20 +7,30 @@ function Home() {
     <div>
       <section>
         <h1>Cognitive Corner</h1>
-        <p>Journey through the minds of the great philosophers who shaped our thoughts and way we think today.
-          <br></br>From Plato to Jacques Derrida, I hope you find the information you are searching for and more! Enlighten yourself!
+        <p>Find out more about the great philosophers who shaped our thoughts and way we think today.
+          <br></br>From Plato to Avicenna, I hope you find the information you are searching for and more! Enlighten yourself!
           <br></br>
         </p>
         <img src={homePageImage} alt="Socrates" className='homeImage'></img>
-        <h2>Explore the ideas and philosophies that shaped our world</h2>
-        <p>The information you will find here are </p>
       </section>
       <section>
         <h4>Recommended Readings</h4>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
+        <p>I encourage you to read more and gain insight from the writings of some of the most influential
+          <br></br>philosophers. Below is a list of books I recommend if you would like to learn more.
+        </p>
+        <ul className='list'>
+          <li>
+            <a href="https://www.goodreads.com/book/show/30292.Five_Dialogues" 
+            className='link'>Five Dialogues: Euthyphro, Apology, Crito, Meno, Phaedo</a>
+          </li>
+          <li>
+            <a href="https://www.goodreads.com/book/show/25709.Summa_Theologica" 
+            className='link'>Summa Theologica</a>
+          </li>
+          <li>
+            <a href="https://www.goodreads.com/book/show/208075.The_Metaphysics_of_The_Healing" 
+            className='link'>The Metaphysics of The Healing</a>
+          </li>
         </ul>
       </section>
     </div>
