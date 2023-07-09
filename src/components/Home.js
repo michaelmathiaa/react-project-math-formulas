74cd2b5ac4aa5e8955bef878c3deee419febb964
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-09 11:26:44 -0700

    Changes sections to header, main, and footer

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,15 +5,17 @@ import homePageImage from "../images/home-page-image.jpg";
 function Home() {
   return (
     <div>
-      <section>
+      <header>
         <h1>Cognitive Corner</h1>
+      </header>
+      <main>
         <p>Find out more about the great philosophers who shaped our thoughts and way we think today.
           <br></br>From Plato to Avicenna, I hope you find the information you are searching for and more! Enlighten yourself!
           <br></br>
         </p>
         <img src={homePageImage} alt="Socrates" className='home-image'></img>
-      </section>
-      <section>
+      </main>
+      <footer>
         <h4>Recommended Readings</h4>
         <p>I encourage you to read more and gain insight from the writings of some of the most influential
           <br></br>philosophers. Below is a list of books I recommend if you would like to learn more.
@@ -32,7 +34,7 @@ function Home() {
             className='link'>The Metaphysics of The Healing</a>
           </li>
         </ul>
-      </section>
+      </footer>
     </div>
   );
 }
